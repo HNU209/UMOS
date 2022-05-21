28105b9fe3fa5785a0cd2515cadb0238b7f31cd6
happy956 <happy8604@naver.com>
2022-05-21 17:35:22 +0900

    add backend files

---
+++ backend/utility.py
@@ -0,0 +1,20 @@
+from azure.storage.blob import BlobServiceClient
+from dotenv import load_dotenv
+import os
+
+folder_loc = os.path.dirname(os.path.abspath(__file__))
+
+load_dotenv()
+
+def get_data(type):
+    client = BlobServiceClient.from_connection_string(os.environ.get('connect_str'))
+    container_client = client.get_container_client(os.environ.get('container_name'))
+    blob_name = os.environ.get(type)
+    blob_client = container_client.get_blob_client(blob_name)
+    
+    if not os.path.isdir(f'{folder_loc}/json'):
+        os.makedirs(f'{folder_loc}/json')
+        
+    with open(f'{folder_loc}/json/{type}.json', 'wb') as f:
+        download_stream = blob_client.download_blob()
+        f.write(download_stream.readall())
