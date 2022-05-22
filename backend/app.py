8aced8eeb039ba33c719fd3ee5c022bff35d93ed
happy956 <happy8604@naver.com>
2022-05-22 18:20:01 +0900

    upload frontend backend

---
+++ backend/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+from utility import main
+from flask_cors import CORS
+import json
+import os
+
+folder_loc = os.path.dirname(os.path.abspath(__file__))
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route(f'/data/<type>')
+def get_json(type):
+    main(type)
+    with open(f'{folder_loc}/json/{type}.json', 'r') as f:
+        data = json.load(f)
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
