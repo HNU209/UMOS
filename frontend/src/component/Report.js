d1a64be7c25fe5625294048ccd3f456dea5a1690
happy956 <happy8604@naver.com>
2022-05-23 19:09:15 +0900

    update frontend

--- frontend/src/component/Report.js
+++ frontend/src/component/Report.js
@@ -1,8 +1,6 @@
-import {useEffect, useState } from 'react';
 import '../css/report.css'
 
 export default function Report(props){
-    const result = props.result
     const time = props.time
 
     const driveTaxiNum = props.driveTaxiNum
