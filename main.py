1847333c5de5bdf92e2bd90cc9175eeb066cc8ec
yh_zoo <dusgurwn@naver.com>
2022-06-01 21:36:26 +0900

    main.py  간단 설명

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from module.dispatch_module import dispatch_module_main
 
+'''
+원하는 날짜 선택 
+승객 증가 비율 선택 (장애인 이동율 0.04)
+승객의 dispatch time 조절 가능 
+'''
 date = "20220216"
 fake_ratio = 0
 fail_time = 30 
