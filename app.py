49ed269c25ba287cfb325b2b34e5f118692fbb98
happy956 <happy8604@naver.com>
2022-06-02 17:35:31 +0900

    rename dash_result.py to app.py

--- app.py
+++ app.py
@@ -567,4 +567,4 @@ def render_page_content(pathname):
 
 
 if __name__ == "__main__":
-    app.run_server()
+    app.run_server(host='0.0.0.0', port=5000)
--- dockerfile
+++ dockerfile
@@ -10,4 +10,4 @@ RUN pip3 install -r requirements.txt
 
 EXPOSE 5000
 
-CMD ["python3", "dash_result.py"]
+CMD ["python3", "app.py"]
