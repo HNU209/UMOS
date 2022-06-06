eae2f3d4750057c0189f5e0f5f25a192b7d02e08
yh_zoo <dusgurwn@naver.com>
2022-06-06 23:50:10 +0900

    dash update

--- app.py
+++ app.py
@@ -30,7 +30,6 @@ drive_tx = dispatch_inf["driving_taxi_num"].tolist()
 trips = load_json_trans_data('trips', data_ct, type="pd")
 trips = [{"vendor":i[1]["vendor"], "type":i[1]["type"], "path":i[1]["path"], "timestamps":i[1]["timestamps"]} for i in trips.iterrows()]
 
-
 ######### Visualization
 import plotly.io as pio
 
@@ -39,30 +38,37 @@ pio.templates["custom_dark"]['layout']['font']['color'] = '#c3c4c7'
 
 ###### Page1
 # 1-a
-page1_1a = f"전체 호출 수 - 총 {len(passenger_locations)}건, 실패 호출 수 - 총 {len(all_fail_data)}건"
+page1_1a = f"전체 호출 수 - 총 {len(passenger_locations)}건, 실패 호출 수 - 총 {len(all_fail_data)}건, 호출 실패율 - {round(len(all_fail_data)/len(passenger_locations)*100,2)}%"
 
 # 1-b1
 bins = [i*60 for i in range(6,31)]
 labels = [i for i in range(6,30)]
 
-fail_passenger_distribution = pd.DataFrame(pd.cut(all_fail_data["call_time"] + 30, bins = bins, labels = labels).value_counts(sort=False)).reset_index()
-call_passenger_distribution = pd.DataFrame(pd.cut(passenger_locations["call_time"], bins = bins, labels = labels).value_counts(sort=False)).reset_index()
+all_fail_data["call_time"] = all_fail_data["call_time"] + 30
+all_fail_data.columns = ["geometry", "fail_time"]
+
+fail_passenger_distribution = pd.DataFrame(pd.cut(all_fail_data["fail_time"], bins = bins, right=False, labels = labels).value_counts(sort=False)).reset_index()
+call_passenger_distribution = pd.DataFrame(pd.cut(passenger_locations["call_time"], bins = bins, right=False, labels = labels).value_counts(sort=False)).reset_index()
+fail_passenger_distribution.columns = ["fail_time", "cnt"]
+call_passenger_distribution.columns = ["call_time", "cnt"]
+
+
 
 page1_1b = go.Figure()
-page1_1b.add_trace(go.Scatter(x=call_passenger_distribution["index"], y=call_passenger_distribution["call_time"],
+page1_1b.add_trace(go.Scatter(x=call_passenger_distribution["call_time"][1:19], y=call_passenger_distribution["cnt"][1:19],
                          mode="lines+markers", 
                          name="콜 승객"))
-page1_1b.add_trace(go.Scatter(x=fail_passenger_distribution["index"], y=fail_passenger_distribution["call_time"],
+page1_1b.add_trace(go.Scatter(x=fail_passenger_distribution["fail_time"][1:19], y=fail_passenger_distribution["cnt"][1:19],
                          mode="lines+markers",
                          name="콜 실패 승객"))
 page1_1b.update_layout(
     xaxis = dict(
         tickmode = 'array',
-        tickvals = [i for i in range(6,30)],
-        ticktext = [f"{i}시" for i in range(6,30)]
+        tickvals = [i for i in range(7,25)],
+        ticktext = [f"{i}시" for i in range(7,25)]
     )
 )
-page1_1b.update_xaxes(range=[5.5, 29.5])
+page1_1b.update_xaxes(range=[6.5, 24.5])
 page1_1b.update_yaxes(
         title_text = "승객수")
 
@@ -71,39 +77,22 @@ page1_1b.update_layout(
     margin={"l":0,"r":0,"b":0,"t":0,"pad":0},
     template="plotly_dark")
 
-# 1-b2
-fail_distribution = pd.DataFrame(pd.cut(all_fail_data["call_time"], bins = bins, labels = labels).value_counts(sort=False)).T
-call_distribution = pd.DataFrame(pd.cut(passenger_locations["call_time"], bins = bins, labels = labels).value_counts(sort=False)).T
-
-fail_distribution_summary = pd.concat([fail_distribution, call_distribution])
-fail_distribution_summary.index = ["실패 승객", "총 승객"]
-fail_distribution_summary.columns = [f"{i}시" for i in fail_distribution_summary.columns]
-page1_1b_summary = fail_distribution_summary
-
-# 2-a
-fail_distribution = pd.DataFrame(pd.cut(all_fail_data["call_time"], bins = bins, labels = labels).value_counts(sort=False)).T
-call_distribution = pd.DataFrame(pd.cut(passenger_locations["call_time"], bins = bins, labels = labels).value_counts(sort=False)).T
-
-fail_distribution_summary = pd.concat([fail_distribution, call_distribution])
-fail_distribution_summary.index = ["실패 승객", "총 승객"]
-fail_distribution_summary.columns = [f"{i}시" for i in fail_distribution_summary.columns]
-page1_1b_summary = fail_distribution_summary
-
 # 2-a
 waiting_time = pd.DataFrame([(ps_final_inf["dispatch_time"] + ps_final_inf["wait_time"]).tolist(), ps_final_inf["time"].tolist()]).T
-waiting_time.columns = ["total_waiting_time", "time"]
+waiting_time.columns = ["waiting_time", "time"]
+waiting_time = waiting_time.loc[waiting_time["time"] <= 1500]
 
-fail_waiting = all_fail_data[["call_time"]]
-fail_waiting["total_waiting_time"] = 30
-fail_waiting.columns = ["time", "total_waiting_time"]
 
-waiting_time = pd.concat([waiting_time, fail_waiting])
-waiting_time["total_waiting_time"] = np.round(waiting_time["total_waiting_time"].values,2)
+fail_waiting = pd.DataFrame(all_fail_data["fail_time"].values - 30, columns=["time"])
+fail_waiting["waiting_time"] = 30
+
+waiting_time = pd.concat([waiting_time, fail_waiting], axis=0).reset_index(drop=True)
+waiting_time["waiting_time"] = np.round(waiting_time["waiting_time"].values,2)
 
 page1_2a = go.Figure()
-page1_2a.add_trace(go.Box(y=waiting_time["total_waiting_time"],hoverinfo='x+y'))
+page1_2a.add_trace(go.Box(y=waiting_time["waiting_time"],hoverinfo='x+y'))
 page1_2a.update_layout(
-    title={'text': f"전체 기간동안의 평균 승객 대기시간 - {round(np.mean(waiting_time['total_waiting_time']))}분",
+    title={'text': f"전체 기간동안의 평균 승객 대기시간 - {round(np.mean(waiting_time['waiting_time']))}분",
            'x':0.5,
            'y':0.9},
     margin={"l":0,"r":0,"b":0,"t":50,"pad":0},
@@ -115,40 +104,45 @@ page1_2a.update_yaxes(
         visible=True)
 
 # 2-b
-waiting_time["time_cut"] = pd.cut(waiting_time["time"], bins = bins, labels = labels).tolist()
+waiting_time["time_cut"] = pd.cut(waiting_time["time"], bins = bins, right=False, labels = labels).tolist()
 
 top_5per_waiting_time = []
 time = []
 for i in waiting_time.groupby(["time_cut"]):
     sample = round(len(waiting_time.loc[waiting_time["time_cut"] == i[0]]) * 0.05)
     sample = 1 if sample < 1 else sample
-    subset = i[1].sort_values("total_waiting_time", ascending=False).head(sample)
-    top_5per_waiting_time.extend([np.mean(subset["total_waiting_time"])])
+    subset = i[1].sort_values("waiting_time", ascending=False).head(sample)
+    top_5per_waiting_time.extend([np.mean(subset["waiting_time"])])
     time.extend([i[0]])
     
 page1_2b = go.Figure()
-page1_2b.add_trace(go.Box(x=waiting_time["time_cut"], y=waiting_time["total_waiting_time"]))
-page1_2b.add_trace(go.Scatter(x=time, y=top_5per_waiting_time))
+page1_2b.add_trace(go.Box(x=waiting_time["time_cut"], y=waiting_time["waiting_time"], showlegend=False))
+page1_2b.add_trace(go.Scatter(x=time, y=top_5per_waiting_time,showlegend=True, name = "상위 5%"))
 page1_2b.update_yaxes(
         title_text = "분")
 page1_2b.update_layout(showlegend=False)
 page1_2b.update_layout(
     xaxis = dict(
         tickmode = 'array',
-        tickvals = [i for i in range(6,30)],
-        ticktext = [f"{i}시" for i in range(6,30)]),
+        tickvals = [i for i in range(7,25)],
+        ticktext = [f"{i}시" for i in range(7,25)]),
+    yaxis = dict(
+        tickmode = 'array',
+        tickvals = [i for i in range(20,121, 20)]),
     title={'text': '시간대별 승객 대기시간',
            'x':0.5,
            'y':0.9},
+    showlegend = True,
+    legend= dict(x=0.9, y=1.1),
     margin={"l":0,"r":0,"b":0,"t":50,"pad":0},
     template="plotly_dark"
 )
 
 ###### Page2
 # 1
-empty_tx_per = [round(i/(i+j), 2) for i,j in zip(empty_tx, drive_tx)]
+empty_tx_per = [round(i/(i+j), 2) for i,j in zip(empty_tx, drive_tx)][60:1081]
 drive_tx_per = [1-i for i in empty_tx_per]
-t = [i for i in range(360, 1801)]
+t = [i for i in range(360, 1801)][60:1081]
 
 page2_1 = go.Figure()
 page2_1.add_trace(go.Bar(
@@ -169,33 +163,30 @@ page2_1.update_layout(
 page2_1.update_layout(
     xaxis = dict(
         tickmode = 'array',
-        tickvals = [i*60 for i in range(6,31)],
-        ticktext = [f"{i}시" for i in range(6,31)]),
+        tickvals = [i*60 for i in range(7,25)],
+        ticktext = [f"{i}시" for i in range(7,25)]),
     yaxis = dict(
         tickmode = 'array',
         tickvals = [0.2, 0.4, 0.6, 0.8, 1.0],
         ticktext = ["20%", "40%", "60%", "80%", "100%"]),
-    margin={"l":0,"r":0,"b":0,"t":0,"pad":0},
+    legend = dict(x=0.9, y=1.1),
+    margin={"l":0,"r":20,"b":0,"t":30,"pad":0},
     template="plotly_dark"
 )
 
-
-
-# 2 
+# 3 
 taxi_final_inf = taxi_final_inf.fillna(0)
 taxi_final_inf["total_drive_time"] = taxi_final_inf["total_to_ps_drive_time"] + taxi_final_inf["total_ps_drive_time"]
 
-page2_2 = f"총 운행 차량 대수 : {len(taxi_final_inf)}대/일\n총 운행 거리 : {round(taxi_final_inf[['total_ps_distance', 'total_ps_distance']].values.sum() / 1000)}km/일"
-
-# 3 
-taxi_driving_nm = (np.array(drive_tx) + np.array(empty_tx)).tolist()
+taxi_driving_nm = (np.array(drive_tx) + np.array(empty_tx)).tolist()[60:1081]
 taxi_driving_nm = taxi_driving_nm[::60]
-taxi_driving_nm_fig = px.bar(x=[i for i in range(6,31)], y=taxi_driving_nm)
+
+taxi_driving_nm_fig = px.bar(x=[i for i in range(7,25)], y=taxi_driving_nm)
 taxi_driving_nm_fig.update_layout(
     xaxis = dict(
         tickmode = 'array',
-        tickvals = [i for i in range(6,31)],
-        ticktext = [f"{round(i)}시" for i in range(6,31)],
+        tickvals = [i for i in range(7,25)],
+        ticktext = [f"{round(i)}시" for i in range(7,25)],
         title=""),
     yaxis = dict(
         tickmode = 'array',
@@ -222,7 +213,7 @@ labels = [i*60 for i in range(6,30)]
 time_list = [i for i in range(420,1441,60)]
 
 # 1_1
-ps_start_inf["time"] = pd.cut(ps_start_inf["start_time"], bins = bins, labels = labels).tolist()
+ps_start_inf["time"] = pd.cut(ps_start_inf["start_time"], right=False, bins = bins, labels = labels).tolist()
 
 start_inf = []
 for i in time_list:
@@ -297,6 +288,8 @@ page3_1_1 = go.Figure(data=data, layout=layout, frames=frames)
 
 
 # 3-1-1-a
+ps_start_inf = ps_start_inf.loc[(ps_start_inf["start_time"] >= 420) & (ps_start_inf["start_time"] <= 1441)]
+
 data = go.Densitymapbox(lat=ps_start_inf.lat, lon=ps_start_inf.long,
                                  radius=7)
 
@@ -313,10 +306,8 @@ layout_basic = go.Layout(
 
 page3_1_1_a = go.Figure(data=data, layout=layout_basic)
 
-
-
 # 1-2
-ps_end_inf["time"] = pd.cut(ps_end_inf["end_time"], bins = bins, labels = labels).tolist()
+ps_end_inf["time"] = pd.cut(ps_end_inf["end_time"], bins = bins, right=False, labels = labels).tolist()
 
 end_inf = []
 for i in time_list:
@@ -339,6 +330,7 @@ data = frames[0]['data']
 # Creating the figure
 page3_1_2 = go.Figure(data=data, layout=layout, frames=frames)
 
+ps_end_inf = ps_start_inf.loc[(ps_end_inf["end_time"] >= 420) & (ps_end_inf["end_time"] <= 1441)]
 
 data = go.Densitymapbox(lat=ps_end_inf.lat, lon=ps_end_inf.long,
                                  radius=7)
@@ -355,12 +347,10 @@ layout_basic = go.Layout(
 )
 
 page3_1_2_a = go.Figure(data=data, layout=layout_basic)
-page3_1_2_a
 
 
 # 2
-all_fail_data["call_time"] = all_fail_data["call_time"] + 30
-all_fail_data["time"] = pd.cut(all_fail_data["call_time"], bins = bins, labels = labels).tolist()
+all_fail_data["time"] = pd.cut(all_fail_data["fail_time"], bins = bins, right=False, labels = labels).tolist()
 
 all_fail_data["lat"] = [i.y for i in all_fail_data["geometry"]]
 all_fail_data["long"] = [i.x for i in all_fail_data["geometry"]]
@@ -369,7 +359,7 @@ fail_inf = []
 for i in time_list:
     subset_fail_inf = all_fail_data.loc[all_fail_data["time"] == i]
     fail_inf.append(subset_fail_inf)
-
+    
 frames = [{   
     'name':f'frame_{idx+7}',
     'data':[{
@@ -391,10 +381,10 @@ hjd_20180401 = load_json_trans_data("hjd_20180401", extradata_ct, type = "gpd")
 ps_wait_inf = []
 for i in trips:
     if i["vendor"] == 1:
-        ps_wait_inf.append([i["timestamps"][-1] - i["timestamps"][0] , Point([i["path"][-1][0], i["path"][-1][1]])])
+        if (i["timestamps"][0] >= 420) & (i["timestamps"][-1] <= 1500):
+            ps_wait_inf.append([i["timestamps"][-1] - i["timestamps"][0] , Point([i["path"][-1][0], i["path"][-1][1]])])
 
 ps_wait_inf = gpd.GeoDataFrame(ps_wait_inf, columns = ["wait_time", "geometry"])
-
 
 ps_wait_inf = gpd.sjoin(ps_wait_inf, hjd_20180401)
 
@@ -430,7 +420,7 @@ CORS(server)
 
 external_stylesheets = ['https://assets.codepen.io/8349312/custom-styles.css']
 
-app = dash.Dash(__name__, server=server, external_stylesheets=external_stylesheets,
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, server=server,
                 meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}])
 
 def build_banner():
@@ -492,11 +482,12 @@ build_tab_1 = [
         html.Div(
             children=[
                  #page1
+                 html.H4(html.B("2022년 02월 14일 07~24시 시뮬레이션 결과"), style={"color": COLORS["text"]}),
                  html.Br(),
                  html.H4(html.B("1. 호출요청 및 호출실패"), style={"color": COLORS["text"]}),
                  html.Br(),
                  #page1-a
-                 dbc.Alert([html.Li(html.B(f"전체 호출 수:  총 {len(passenger_locations)}건 | 전체 실패 호출 수:  총 {len(all_fail_data)}건"),
+                 dbc.Alert([html.Li(html.B(f"전체 호출 수:  총 {len(passenger_locations)}건 | 전체 실패 호출 수:  총 {len(all_fail_data)}건 | 호출 실패율 : {round(len(all_fail_data)/len(passenger_locations)*100,2)}%"),
                                     style={'font-size':'130%',"color": COLORS["text"]})], color="#3c434a"),
                  #page1-b
                  html.Li(html.B("시간대 별 효출요청 및 호출실패 건수"),style={"font-size": '130%', "color": COLORS["text"]}),
@@ -553,7 +544,7 @@ build_tab_3 = [
                  html.Hr(style={'color': COLORS["text"]}),
                  #page3-2
                  dbc.Row([html.H4(html.B("2. 시간대별 배차 실패지점 위치"), style={'color': COLORS["text"], 'width': '50%', 'display': 'inline-block'}),
-                          html.H4(html.B("3. 시간대별 읍면동별 승객 대기시간 분포"), style={'color': COLORS["text"], 'width': '50%', 'display': 'inline-block'})]),
+                          html.H4(html.B("3. 읍면동별 승객 대기시간 분포"), style={'color': COLORS["text"], 'width': '50%', 'display': 'inline-block'})]),
                  html.Br(),
                  dbc.Row([dcc.Graph(figure=page3_2, style={'width': '50%', 'display': 'inline-block', 'padding': '10 0 0 0'}),
                           dcc.Graph(figure=page3_3, style={'width': '50%', 'display': 'inline-block', 'padding': '0 0 0 10'})]),
